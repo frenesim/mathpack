ebc3464f202dfe94cd8f0bc49dfc624f2de3699f
maxmilan <max231194@gmail.com>
2014-12-30 07:01:01 +0300

    Add equation solve function

--- spec/equation_spec.rb
+++ spec/equation_spec.rb
@@ -1,11 +1,14 @@
 describe 'Equations' do
   require 'mathpack/equation'
 
-  context "calculated unlinear equations" do
+  context 'calculate nonlinear equations' do
 
     it 'should solve equation' do
       eps = 0.00001
-      expect(Mathpack::Equation.solve(0, eps){|x| x**2 - Math.sin(x+1)}).to be_between(-0.613763 - eps, -0.613763 + eps)
+      expect(Mathpack::Equation.solve(start: 0, eps: eps){|x| x**2 - Math.sin(x+1)}).to be_between(-0.613763 - eps, -0.613763 + eps)
+      expect(Mathpack::Equation.solve(start: 0.01, eps: eps){|x| 1/x - Math.log(x)}).to be_between(1.76322 - eps, 1.76322 + eps)
+      expect(Mathpack::Equation.solve(start: 0.01, eps: eps){|x| x**2 - 2*x + 1}).to    be_between(1.0 - eps, 1.0 + eps)
+      expect(Mathpack::Equation.solve(start: 0.01, eps: eps){|x| Math.exp(x-2) - Math.sin(x)}).to    be_between(0.159396 - eps, 0.159396 + eps)
     end
   end
 end
